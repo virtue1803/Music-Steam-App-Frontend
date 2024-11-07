66ee02f12b6caf51a11ff765903591c8e55abbb4
virtue1803 <nguyenductai505@gmail.com>
2024-11-07 13:00:24 +0700

    update search.js

--- music_stream_app/Screens/Search.js
+++ music_stream_app/Screens/Search.js
@@ -6,31 +6,31 @@ const SearchResultsScreen = () => {
   const [selectedTab, setSelectedTab] = useState('All');
   const navigation = useNavigation();
 
-  // Dữ liệu giả cho từng tab với URL nhạc
+  // Dữ liệu giả cho từng tab với URL nhạc và ảnh cục bộ
   const tracksData = [
-    { id: '1', title: 'City of Stars', artist: 'Ryan Gosling ft. Emma Stone', plays: '2.1M', duration: '3:36', url: 'https://www.nhaccuatui.com/bai-hat/city-of-stars-from-la-la-land-soundtrack-ryan-gosling-ft-emma-stone.tRE4ZrubnEzb.html' },
-    { id: '2', title: 'Shallow', artist: 'Lady Gaga ft. Bradley Cooper', plays: '1.5M', duration: '3:45', url: 'https://www.nhaccuatui.com/bai-hat/shallow-radio-edit-lady-gaga-ft-bradley-cooper.6IL4W7BvPzm0.html' },
-    { id: '3', title: 'Always Remember Us This Way', artist: 'Lady Gaga', plays: '3.2M', duration: '4:00', url: 'https://www.nhaccuatui.com/bai-hat/always-remember-us-this-way-lady-gaga.kr64z1ZHGyf5.html' },
-    { id: '4', title: 'My Heart Will Go On', artist: 'Celine Dion', plays: '900K', duration: '3:21', url: 'https://www.nhaccuatui.com/bai-hat/my-heart-will-go-on-love-theme-from-titanic-celine-dion.hd94Jl6xoZix.html' },
-    { id: '5', title: 'Speechless', artist: 'Naomi Scott', plays: '1.2M', duration: '2:58', url: 'https://www.nhaccuatui.com/bai-hat/speechless-full-from-aladdinsoundtrack-version-naomi-scott.ag7rg21KcX18.html' },
+    { id: '1', title: 'City of Stars', artist: 'Ryan Gosling ft. Emma Stone', plays: '2.1M', duration: '3:36', url: 'https://www.nhaccuatui.com/bai-hat/city-of-stars-from-la-la-land-soundtrack-ryan-gosling-ft-emma-stone.tRE4ZrubnEzb.html', image: require('../assets/Audio Listing - Search Results/Image 83.png') },
+    { id: '2', title: 'Shallow', artist: 'Lady Gaga ft. Bradley Cooper', plays: '1.5M', duration: '3:45', url: 'https://www.nhaccuatui.com/bai-hat/shallow-radio-edit-lady-gaga-ft-bradley-cooper.6IL4W7BvPzm0.html', image: require('../assets/Audio Listing - Search Results/Image 84.png') },
+    { id: '3', title: 'Always Remember Us This Way', artist: 'Lady Gaga', plays: '3.2M', duration: '4:00', url: 'https://www.nhaccuatui.com/bai-hat/always-remember-us-this-way-lady-gaga.kr64z1ZHGyf5.html', image: require('../assets/Audio Listing - Search Results/Image 85.png') },
+    { id: '4', title: 'My Heart Will Go On', artist: 'Celine Dion', plays: '900K', duration: '3:21', url: 'https://www.nhaccuatui.com/bai-hat/my-heart-will-go-on-love-theme-from-titanic-celine-dion.hd94Jl6xoZix.html', image: require('../assets/Audio Listing - Search Results/Image 86.png') },
+    { id: '5', title: 'Speechless', artist: 'Naomi Scott', plays: '1.2M', duration: '2:58', url: 'https://www.nhaccuatui.com/bai-hat/speechless-full-from-aladdinsoundtrack-version-naomi-scott.ag7rg21KcX18.html', image: require('../assets/Audio Listing - Search Results/Image 87.png') },
   ];
 
   const albumsData = [
-    { id: '6', title: 'Dance The Night', artist: 'Dua Lipa', plays: '4.3M', duration: '3:50', url: 'https://www.nhaccuatui.com/bai-hat/dance-the-night-from-barbie-the-album-dua-lipa.e2dypeo6Zpg5.html' },
-    { id: '7', title: 'If I Ain\'t Got You', artist: 'Alicia Keys', plays: '2.9M', duration: '4:10', url: 'https://www.nhaccuatui.com/bai-hat/if-i-aint-got-you-orchestral-alicia-keys-ft-queen-charlottes-global-orchestra.YD7b9iiCqe5u.html' },
-    { id: '8', title: 'Here I Am', artist: 'Dolly Parton ft. Sia', plays: '5.1M', duration: '3:40', url: 'https://www.nhaccuatui.com/bai-hat/here-i-am-from-the-dumplin-original-motion-picture-soundtrack-dolly-parton-ft-sia.6VoDbMWV8Wbo.html' },
+    { id: '6', title: 'Dance The Night', artist: 'Dua Lipa', plays: '4.3M', duration: '3:50', url: 'https://www.nhaccuatui.com/bai-hat/dance-the-night-from-barbie-the-album-dua-lipa.e2dypeo6Zpg5.html', image: require('../assets/Audio Listing - Search Results/Image 88.png') },
+    { id: '7', title: 'If I Ain\'t Got You', artist: 'Alicia Keys', plays: '2.9M', duration: '4:10', url: 'https://www.nhaccuatui.com/bai-hat/if-i-aint-got-you-orchestral-alicia-keys-ft-queen-charlottes-global-orchestra.YD7b9iiCqe5u.html', image: require('../assets/Audio Listing - Search Results/Image 89.png') },
+    { id: '8', title: 'Here I Am', artist: 'Dolly Parton ft. Sia', plays: '5.1M', duration: '3:40', url: 'https://www.nhaccuatui.com/bai-hat/here-i-am-from-the-dumplin-original-motion-picture-soundtrack-dolly-parton-ft-sia.6VoDbMWV8Wbo.html', image: require('../assets/Audio Listing - Search Results/Image 90.png') },
   ];
 
   const artistsData = [
-    { id: '9', title: 'Artist Track One', artist: 'Jessica Gonzalez', plays: '2.1M', duration: '3:36', url: 'https://www.nhaccuatui.com/bai-hat/city-of-stars-from-la-la-land-soundtrack-ryan-gosling-ft-emma-stone.tRE4ZrubnEzb.html' },
-    { id: '10', title: 'Artist Track Two', artist: 'Anthony Taylor', plays: '2.8M', duration: '3:55', url: 'https://www.nhaccuatui.com/bai-hat/shallow-radio-edit-lady-gaga-ft-bradley-cooper.6IL4W7BvPzm0.html' },
-    { id: '11', title: 'Artist Track Three', artist: 'Brian Bailey', plays: '3.5M', duration: '4:22', url: 'https://www.nhaccuatui.com/bai-hat/always-remember-us-this-way-lady-gaga.kr64z1ZHGyf5.html' },
+    { id: '9', title: 'Artist Track One', artist: 'Jessica Gonzalez', plays: '2.1M', duration: '3:36', url: 'https://www.nhaccuatui.com/bai-hat/city-of-stars-from-la-la-land-soundtrack-ryan-gosling-ft-emma-stone.tRE4ZrubnEzb.html', image: require('../assets/Audio Listing - Search Results/Image 83.png') },
+    { id: '10', title: 'Artist Track Two', artist: 'Anthony Taylor', plays: '2.8M', duration: '3:55', url: 'https://www.nhaccuatui.com/bai-hat/shallow-radio-edit-lady-gaga-ft-bradley-cooper.6IL4W7BvPzm0.html', image: require('../assets/Audio Listing - Search Results/Image 84.png') },
+    { id: '11', title: 'Artist Track Three', artist: 'Brian Bailey', plays: '3.5M', duration: '4:22', url: 'https://www.nhaccuatui.com/bai-hat/always-remember-us-this-way-lady-gaga.kr64z1ZHGyf5.html', image: require('../assets/Audio Listing - Search Results/Image 85.png') },
   ];
 
   const allData = [
     ...tracksData,
     ...albumsData,
-    ...artistsData.slice(0, 3), // Chỉ lấy một bài hát từ từng nghệ sĩ trong Artists
+    ...artistsData.slice(0, 3),
   ];
 
   const getData = () => {
@@ -76,7 +76,7 @@ const SearchResultsScreen = () => {
             onPress={() => navigation.navigate('PlayAudio', item)}
             style={{ flexDirection: 'row', padding: 10, alignItems: 'center' }}
           >
-            <Image source={{ uri: 'https://picsum.photos/200' }} style={{ width: 50, height: 50, borderRadius: 8 }} />
+            <Image source={item.image} style={{ width: 50, height: 50, borderRadius: 8 }} />
             <View style={{ marginLeft: 10 }}>
               <Text>{item.title}</Text>
               <Text>{item.artist}</Text>
