f52cd33404a54e23fdd8cfd023a17d88b682ab84
Nguyễn Đức Vinh <125174969+NguyenDucVinh-18@users.noreply.github.com>
2024-12-04 13:39:21 +0700

    Create artistProfiles.js
    add dataArtistProfiles

---
+++ music_stream_app/data/artistProfiles.js
@@ -0,0 +1,52 @@
+// data/artistProfiles.js
+export const artistProfiles = [
+  {
+    id: 1,
+    name: 'HIEUTHUHAI',
+    image:
+      'https://kenh14cdn.com/203336854389633024/2024/10/2/image2-1630-1727833092698-17278330935461724047371.jpg',
+    followers: '120k Followers',
+  },
+  {
+    id: 2,
+    name: 'SƠN TÙNG M-TP',
+    image:
+      'https://yt3.googleusercontent.com/oN0p3-PD3HUzn2KbMm4fVhvRrKtJhodGlwocI184BBSpybcQIphSeh3Z0i7WBgTq7e12yKxb=s900-c-k-c0x00ffffff-no-rj',
+    followers: '95k Followers',
+  },
+  {
+    id: 3,
+    name: 'MONO',
+    image:
+      'https://thanhnien.mediacdn.vn/Uploaded/caotung/2022_08_07/ca-si-mono-2225.jpg',
+    followers: '150k Followers',
+  },
+  {
+    id: 4,
+    name: 'Binz',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2GpnkIXWIPAq_bx-KRoCaCvv2sP1_8VkCTEwPNRHbPdHruaJX58sVNkcJ96pRDD4VughaBO75rhzplIE7dnDMTQ',
+    followers: '150k Followers',
+  },
+  {
+    id: 5,
+    name: 'Tăng Duy Tân',
+    image:
+      'https://images2.thanhnien.vn/zoom/686_429/528068263637045248/2024/2/2/tang-duy-tan2-17068758686511859985241-129-0-729-960-crop-17068769899091004605475.jpg',
+    followers: '150k Followers',
+  },
+  {
+    id: 6,
+    name: 'Phan Mạnh Quỳnh',
+    image:
+      'https://thanhnien.mediacdn.vn/Uploaded/haoph/2022_06_07/phan-manh-quynh-5-4064.jpeg',
+    followers: '150k Followers',
+  },
+  {
+    id: 7,
+    name: 'Hồ Ngọc Hà',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUMfT79t6sHlIxXPyyBH7g44zTAAUK26SB3sDvme3kcz_FBx7Meu4JD_uVs8Sts9nQMgAeFJKsRqzFMxrkS-89TA',
+    followers: '150k Followers',
+  },
+];
