f8d4ac7c721e27d717e958fc911b9003d7b4fff7
Nguyễn Đức Vinh <125174969+NguyenDucVinh-18@users.noreply.github.com>
2024-12-04 20:08:24 +0700

    Create albums.js
    add data albums

---
+++ music_stream_app/data/albums.js
@@ -0,0 +1,26 @@
+export const albums = [
+  {
+    idAlbum: 1,
+    idArtist: 1,
+    idSongs: [1, 2, 3, 4, 5, 6],
+    name: 'Ai cũng phải bắt đầu từ đâu đó',
+    image:
+      'https://photo-resize-zmp3.zadn.vn/w600_r1x1_jpeg/cover/d/4/a/c/d4acc6335d41bd7164173312c6123706.jpg',
+  },
+  {
+    idAlbum: 2,
+    idArtist: 5,
+    idSongs: [43,44,45,46,47],
+    name: 'Khu vườn tình',
+    image:
+      'https://i.scdn.co/image/ab67616d0000b2737cdefbd178de8d2e9c907e67',
+  },
+  {
+    idAlbum: 3,
+    idArtist: 3,
+    idSongs: [18,22,25],
+    name: 'EP Đẹp',
+    image:
+      'https://photo-resize-zmp3.zadn.vn/w600_r1x1_jpeg/cover/3/2/f/d/32fda23217e7169c4a4e4e5b4dd4f7fa.jpg',
+  },
+];
